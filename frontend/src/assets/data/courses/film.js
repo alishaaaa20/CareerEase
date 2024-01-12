fa968dd6026e03b5be21c14ad1153ef8e6c27845
unknown <aashika.191501@ncit.edu.np>
2024-01-12 12:11:40 +0545

    courses

--- frontend/src/assets/data/courses/film.js
+++ frontend/src/assets/data/courses/film.js
@@ -1,6 +1,6 @@
 export const film = [
   {
-    name: "Bachelor in film Studies",
+    name: "Bachelor in film Studies(BFS)",
     desc: "A 4 year course is a program that provide students with a comprehensive understanding of the history, theory, and practical aspects of filmmaking.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
--- frontend/src/assets/data/courses/technology.js
+++ frontend/src/assets/data/courses/technology.js
@@ -1,18 +1,18 @@
 export const technology = [
   {
-    name: "Bachelor of Computer Application",
+    name: "Bachelor of Computer Application(BCA)",
     desc: " A 4 year course that is an undergraduate degree program that focuses on computer applications and software development. ",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
   },
   {
-    name: "Bachelor in computer science technology ",
+    name: "Bachelor in computer science technology(Bsc CSIT) ",
     desc: "A 4 year course course that is a blend of both Computer Science and Information Technology. ",
     bgColor: "rgba(151, 113, 255, .2)",
     textColor: "#9771FF",
   },
   {
-    name: "Bachelor of Information Technology",
+    name: "Bachelor of Information Technology(BIT)",
     desc: "A four year course  that is deals with networking, data communication, computer system.",
     bgColor: "rgba(1, 181, 197, .2)",
     textColor: "#01B5C5",
