311f33d1ba9fb7cd22586989beae13c92cc7caa3
Alisha Khatri <alisha.191502@ncit.edu.np>
2024-01-12 08:39:41 +0545

    courses of each field page setup

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -8,6 +8,14 @@ import Dashboard from "../admin/Dashboard";
 import { Routes, Route } from "react-router-dom";
 import QA from "../pages/QA/QA";
 import Medical from "../pages/courses/Medical";
+import Engineering from "../pages/courses/Engineering";
+import Management from "../pages/courses/Management";
+import Humanities from "../pages/courses/Humanities";
+import Art from "../pages/courses/Art";
+import Fashion from "../pages/courses/Fashion";
+import Interior from "../pages/courses/Interior";
+import Film from "../pages/courses/Film";
+import Technology from "../pages/courses/Technology";
 
 const Router = () => {
   return (
@@ -20,6 +28,15 @@ const Router = () => {
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
       <Route path="/services/medical" element={<Medical />} />
+      <Route path="/services/engineering" element={<Engineering />} />
+
+      <Route path="/services/management" element={<Management />} />
+      <Route path="/services/humanities" element={<Humanities />} />
+      <Route path="/services/art" element={<Art />} />
+      <Route path="/services/fashion" element={<Fashion />} />
+      <Route path="/services/film" element={<Film />} />
+      <Route path="/services/interior" element={<Interior />} />
+      <Route path="/services/technology" element={<Technology />} />
       <Route path="/userdashboard" element={<UserDashboard />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
