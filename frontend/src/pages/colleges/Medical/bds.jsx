ad88e9c1ee3b7c60dc7ab97654a328c1791f6c6d
Crotalus-atrox <crestacreeya740@gmail.com>
2024-01-12 14:30:08 +0545

    bds

--- frontend/src/pages/colleges/Medical/bds.jsx
+++ frontend/src/pages/colleges/Medical/bds.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import pulogo from "../../../assets/images/purb.png";
 import tulogo from "../../../assets/images/tu.jpg";
 import kulogo from "../../../assets/images/kulogo.png";
 
@@ -14,12 +14,20 @@ const bds = () => {
         <p className="text__para text-center">
           List of courses available in the Medical field.
         </p>
-        <div className="flex">
-          <div className="xl:w-[470px] flex-1">
-            <h1 className="text-left text-primaryColor mx-auto font-bold font-size-400 mt-[50px] ml-[100px]">
-              TU Affiliated Colleges
-            </h1>
-            <ul className="mt-[50px] font-bold ml-[40px]">
+      </section>
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+            {/*-----makeup artist image-----*/}
+            <div className="relative w-3/4 lg:w-1/2 xl:w-[600px] z-10 order-2 lg:order-1 ml-[50px] mt-[60px]">
+              <img src={tulogo} alt="" height={300} width={300} />
+            </div>
+
+            <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2 ">
+              <h1 className="text-center text-primaryColor flex font-bold  mt-[50px] ml-[25px] text-xl">
+                TU Affiliated Colleges
+              </h1>
+              <ul className="mt-[50px] font-bold ml-[40px]">
               <li className="text__para text-left">
                 <a
                   href="http://iom.edu.np/"
@@ -29,6 +37,15 @@ const bds = () => {
                   Universal Colleges of Medical Sciences
                 </a>
               </li>
+              <li className="text__para">
+                  <a
+                    href="https://www.bpkihs.edu/"
+                    target="_blank"
+                    className="text__link"
+                  >
+                    B.P. Koirala Institute of Health and Sciences (Public)
+                  </a>
+                </li>
               <li className="text__para">
                 <a
                   href="https://www.bpkihs.edu/"
@@ -65,22 +82,23 @@ const bds = () => {
                   Gandaki Medical College
                 </a>
               </li>
-            </ul>
+              </ul>
+            </div>
           </div>
-          <div className="xl:w-[470px] flex-1">
-            <h1 className="text-left text-primaryColor mx-auto font-bold font-size-400 mt-[50px] ml-[190px]">
-              KU Affiliated Colleges
-            </h1>
-            <ul className="mt-[50px] font-bold ml-[40px] text-left ml-[100px]">
-              <li className="text__para">
-                <a
-                  href="https://manipal.edu/mu.html"
-                  target="_blank"
-                  className="text__link"
-                >
-                  Nobel Medical College
-                </a>
-              </li>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+            {/*-----makeup artist image-----*/}
+            <div className="relative w-3/4 lg:w-1/2 xl:w-[600px] z-10 order-2 lg:order-1 ml-[100px] mt-[100px] ">
+              <img src={kulogo} alt="" height={300} width={300} />
+            </div>
+            <div className="xl:w-[470px] ">
+              <h1 className=" text-center text-primaryColor flex font-bold  mt-[50px] ml-[40px] text-xl">
+                KU Affiliated Colleges
+              </h1>
+              <ul className="mt-[50px] font-bold ml-[40px] text-left ml-[100px]">
               <li className="text__para">
                 <a
                   href="https://cmsnepal.edu.np/"
@@ -126,16 +144,8 @@ const bds = () => {
                   Nobel Medical College (Private)
                 </a>
               </li>
-              <li className="text__para">
-                <a
-                  href="https://lmc.edu.np/"
-                  target="_blank"
-                  className="text__link"
-                >
-                  Lumbini Medical College (Private)
-                </a>
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
@@ -144,3 +154,4 @@ const bds = () => {
 };
 
 export default bds;
+
