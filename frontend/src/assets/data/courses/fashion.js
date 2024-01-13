160df380a3d68de385523080e603bfc51f1fac26
unknown <aashika.191501@ncit.edu.np>
2024-01-13 08:27:50 +0545

    courses all

--- frontend/src/assets/data/courses/fashion.js
+++ frontend/src/assets/data/courses/fashion.js
@@ -4,6 +4,7 @@ export const fashion = [
     desc: "A 3-4 year course that is an undergraduate degree program that provides students with a comprehensive understanding of the fashion industry.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
+    link: "/services/fashion/bfd",
   },
  
 ];
--- frontend/src/assets/data/courses/interior.js
+++ frontend/src/assets/data/courses/interior.js
@@ -4,5 +4,6 @@ export const interior = [
     desc: "3 years to learn interior designing through progressive briefs that engage with real-world challenges, through research, and through designs.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
+    link: "/services/interior/bid",
   },
 ];
